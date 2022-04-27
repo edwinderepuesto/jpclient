4f193d1de07c151febe54de30cb6d1280b7f7841
Edwin González <edwinderepuesto@gmail.com>
2022-04-26 22:34:27 -0400

    Refactored repository

--- app/src/main/java/com/edwinderepuesto/jpclient/data/repository/MainRepository.kt
+++ app/src/main/java/com/edwinderepuesto/jpclient/data/repository/MainRepository.kt
@@ -1,64 +1,31 @@
 package com.edwinderepuesto.jpclient.data.repository
 
-import android.content.Context
-import android.util.Log
-import androidx.room.Room
 import com.edwinderepuesto.jpclient.data.api.JsonPlaceholderApi
-import com.edwinderepuesto.jpclient.data.database.AppDatabase
+import com.edwinderepuesto.jpclient.data.dao.PostDao
 import com.edwinderepuesto.jpclient.data.dto.Post
 import com.edwinderepuesto.jpclient.data.dto.PostComment
 import com.edwinderepuesto.jpclient.data.dto.User
-import io.ktor.client.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
-import io.ktor.serialization.kotlinx.json.*
-import kotlinx.serialization.json.Json
-import java.lang.ref.WeakReference
 
 class MainRepository(
-    contextRef: WeakReference<Context>,
+    private val api: JsonPlaceholderApi,
+    private val postDao: PostDao
 ) {
-    private val postDao = Room.databaseBuilder(
-        contextRef.get()!!,
-        AppDatabase::class.java, "jpclient-db"
-    ).build().postDao()
-
-    private val httpClient = HttpClient {
-        install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                isLenient = true
-                ignoreUnknownKeys = true
-            })
-        }
-        install(Logging) {
-            logger = object : Logger {
-                override fun log(message: String) {
-                    Log.v("Ktor Logger ->", message)
-                }
-            }
-            level = LogLevel.ALL
-        }
-    }
-
-    private val jsonPlaceholderApi = JsonPlaceholderApi(httpClient)
-
     suspend fun getPosts(): List<Post> {
         val storedPosts = postDao.loadAllPosts()
 
         return storedPosts.ifEmpty {
-            val posts = jsonPlaceholderApi.getPosts()
+            val posts = api.getPosts()
             postDao.insertAll(posts)
             posts
         }
     }
 
     suspend fun getCommentsByPostId(postId: Int): List<PostComment> {
-        return jsonPlaceholderApi.getCommentsByPostId(postId)
+        return api.getCommentsByPostId(postId)
     }
 
     suspend fun getUserById(userId: Int): User {
-        return jsonPlaceholderApi.getUserById(userId)
+        return api.getUserById(userId)
     }
 
     fun updatePost(post: Post) {
--- app/src/main/java/com/edwinderepuesto/jpclient/di/AppModule.kt
+++ app/src/main/java/com/edwinderepuesto/jpclient/di/AppModule.kt
@@ -1,13 +1,21 @@
 package com.edwinderepuesto.jpclient.di
 
 import android.content.Context
+import android.util.Log
+import androidx.room.Room
+import com.edwinderepuesto.jpclient.data.api.JsonPlaceholderApi
+import com.edwinderepuesto.jpclient.data.database.AppDatabase
 import com.edwinderepuesto.jpclient.data.repository.MainRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import java.lang.ref.WeakReference
+import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @Module
@@ -16,6 +24,31 @@ object AppModule {
     @Provides
     @Singleton
     fun provideMainRepository(@ApplicationContext appContext: Context): MainRepository {
-        return MainRepository(WeakReference(appContext))
+        val httpClient = HttpClient {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                })
+            }
+            install(Logging) {
+                logger = object : Logger {
+                    override fun log(message: String) {
+                        Log.v("Ktor Logger ->", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+        }
+
+        val api = JsonPlaceholderApi(httpClient)
+
+        val postDao = Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java, "jpclient-db"
+        ).build().postDao()
+
+        return MainRepository(api, postDao)
     }
 }
